2677c11d7588f285bc1b0a15ab0390acce8538d9
Aditya Pradhan <142919932+AdiInfiniteLoop@users.noreply.github.com>
2024-07-28 10:43:01 +0530

    Delete UI directory

--- UI/src/components/CopyRight.jsx
+++
@@ -1,9 +0,0 @@
-const CopyRight = () => {
-  return (
-    <div className="text-sm text-gray-600 mb-4">
-      © 2024 JPMC Code For Good. All rights reserved.
-    </div>
-  );
-};
-
-export default CopyRight;
